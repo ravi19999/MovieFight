5d5363e6a6f5089574845c54f6fd2909e2c01dab
ravi19999 <dahalravindra@gmail.com>
2020-06-19 16:32:30 +0545

    Connected to the OMDB API via Axios

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd19d1f9b',
+            s: "avengers"
+        }
+    });
+
+    console.log(response.data);
+};
+
+fetchData();
