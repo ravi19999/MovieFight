601a891056327877d96a696145422b54aaebe667
ravi19999 <dahalravindra@gmail.com>
2020-06-22 00:23:31 +0545

    Fixed the 'movie not found' bug andported most of the HTML to be generated dynamically by JavaScript

--- index.js
+++ index.js
@@ -13,20 +13,37 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+       </div>
+    </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector('.results')
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
 
+    resultsWrapper.innerHTML = ``;
     for (let movie of movies) {
-        const div = document.createElement('div');
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-        div.innerHTML = `
-        <img src="${movie.Poster}" />
-        <h1>${movie.Title}</h1>
+        option.classList.add('dropdown-item');
+        dropdown.classList.add('is-active');
+        option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
         `;
 
-        document.querySelector('#target').appendChild(div);
+        resultsWrapper.appendChild(option);
     }
 };
 input.addEventListener('input', debounce(onInput, 500));
